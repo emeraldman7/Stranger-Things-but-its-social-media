c5d5776cb515f9434c71946d7546ce0534d99e0b
emeraldman7 <drewiggy19@gmail.com>
2023-04-30 00:38:56 -0500

    register setup complete

--- src/components/Register.js
+++ src/components/Register.js
@@ -1,10 +1,26 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 function Register(){
+
+    const [username, setUsername] = useState('');
+    const [password, setPassword] = useState('');
+
+    console.log(username, password)
+
     return(
-        <h1>
-            REGISTER!!!!!!!
-        </h1>
+        <form>
+            <input
+            type="text"
+            placeholder="enter username"
+            onChange={(eventUser) => setUsername(eventUser.target.value)}
+            />
+            <input
+            type="password"
+            placeholder="enter password"
+            onChange={(eventPass) => setPassword(eventPass.target.value)}
+            />
+        <button type="Log in">Log in</button>
+        </form>
     )
 }
 
